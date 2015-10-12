0be99122c9592af7e667434c527c993b059a3140
George Toothman <george.toothman@flatironschool.com>
2015-10-12 11:38:25 -0400

    add loop for calculating fibonacci

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,19 @@
 # Implement your procedural solution here!
+# create an array beginning with 1 and 2
+# loop through the array indexes to create the index 2 number which is three
+# break if current index position >= 4,000,000
+# add numbers in a new_array that have no modulo remainder 
+
+array = [*1..2]
+position_one = 0
+position_two = 1
+
+loop do
+  first_index = array.index(array[position_one])
+  second_index = array.index(array[position_two])
+  new_sequence_item = array[first_index] + array[second_index]
+  array.push(new_sequence_item)
+  position_one += 1
+  position_two += 1
+  break if position_one == 20
+end
